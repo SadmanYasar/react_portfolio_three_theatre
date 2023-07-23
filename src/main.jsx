686c0e6ef0b45d11add4e7712af2f20cb850c9d3
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-07-23 08:54:33 +0800

    remove editor

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-studio.extend(extension);
-studio.initialize();
+// studio.extend(extension);
+// studio.initialize();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
