5c438731f711c04976d018daf452db776dd9435b
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-07-26 01:10:20 +0800

    update seo

---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import "./Tooltip.css";
+
+const ToolTip = ({ text }) => {
+    return (
+        <div className='scroll-container'>
+            <div className='icon-scroll'>
+            </div>
+        </div>
+    );
+}
+
+export default ToolTip;
