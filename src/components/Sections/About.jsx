a68ceb4d61c11fd284bdd53ad83273fb617812e8
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-07-28 02:17:59 +0800

    update image url

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -1,11 +1,9 @@
-const img = 'https://media.licdn.com/dms/image/D5603AQEjwJTzxoowUg/profile-displayphoto-shrink_800_800/0/1683264970160?e=2147483647&v=beta&t=80g7pjhga2ytWSd020WK9sjNTvZKNeUh3nDkfZGC0YY'
-
 export default function About() {
     return (
         <section className="w-full min-h-screen max-w-5xl mx-auto px-4">
             <div className="flex flex-col">
                 <div className="p-1 rounded-full animate-text bg-gradient-to-r from-teal-300 via-purple-500 to-orange-500 self-center">
-                    <img src={img} className="w-64 h-64 object-cover rounded-full" />
+                    <img src={'/profile_pic_sadman_yasar_sayem.jpeg'} className="w-64 h-64 object-cover rounded-full" />
                 </div>
                 <div className="text-3xl md:text-4xl text-center font-bold pt-12">
                     Full Stack Web Developer
