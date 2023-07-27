d322fc616f86de83ccbbfa14ab0517b0756c92e1
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-07-27 13:44:50 +0800

    added skills

--- src/components/Sections/TechStacks.jsx
+++ src/components/Sections/TechStacks.jsx
@@ -1,57 +1,42 @@
-import { useEffect, useRef, useState } from "react";
-import { motion, useScroll, wrap, useSpring, useVelocity, useTransform } from "framer-motion";
-import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-
-gsap.registerPlugin(ScrollSmoother);
-
-const techIcons = ['🚀', '🚗', '⚛', '🏀', '🥎', '🚀', '🚗', '⚛', '🏀', '🥎'];
+import { useRef } from "react";
+import {
+    motion,
+    useScroll,
+    useSpring,
+    useTransform,
+    useMotionValue,
+    useVelocity,
+    useAnimationFrame
+} from "framer-motion";
+import { wrap } from "@motionone/utils";
 
 function ParallaxText({ children, baseVelocity = 100 }) {
-    const [numOfChildren, setNumOfChildren] = useState(4); // Default number of children
-    const scrollerRef = useRef(null);
-
-    useEffect(() => {
-        // Calculate the number of times to repeat the child text dynamically
-        const scrollerWidth = scrollerRef.current.offsetWidth;
-        const textWidth = scrollerWidth / techIcons.length;
-        const newNumOfChildren = Math.ceil(textWidth / children.length);
-        setNumOfChildren(newNumOfChildren);
-
-        // Initialize ScrollSmoother with the provided options
-        const smoother = ScrollSmoother.create({
-            content: scrollerRef.current,
-            wrapper: scrollerRef.current.parentNode,
-            smooth: 1,
-            effects: true,
-            normalizeScroll: true,
-            ignoreMobileResize: true,
-        });
-
-        return () => {
-            smoother.kill(); // Cleanup ScrollSmoother when the component unmounts
-        };
-    }, [children.length]);
-
     const baseX = useMotionValue(0);
     const { scrollY } = useScroll();
     const scrollVelocity = useVelocity(scrollY);
     const smoothVelocity = useSpring(scrollVelocity, {
         damping: 50,
-        stiffness: 400,
+        stiffness: 400
     });
     const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-        clamp: false,
+        clamp: false
     });
 
-    const totalWidth = (-100 / techIcons.length) * numOfChildren;
-
-    const x = useTransform(baseX, (v) => `${wrap(0, totalWidth, v)}%`);
+    /**
+     * This is a magic wrapping for the length of the text - you
+     * have to replace for wrapping that works for you or dynamically
+     * calculate
+     */
+    const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
 
     const directionFactor = useRef(1);
     useAnimationFrame((t, delta) => {
         let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
 
+        /**
+         * This is what changes the direction of the scroll once we
+         * switch scrolling directions.
+         */
         if (velocityFactor.get() < 0) {
             directionFactor.current = -1;
         } else if (velocityFactor.get() > 0) {
@@ -63,12 +48,20 @@ function ParallaxText({ children, baseVelocity = 100 }) {
         baseX.set(baseX.get() + moveBy);
     });
 
+    /**
+     * The number of times to repeat the child text should be dynamically calculated
+     * based on the size of the text and viewport. Likewise, the x motion value is
+     * currently wrapped between -20 and -45% - this 25% is derived from the fact
+     * we have four children (100% / 4). This would also want deriving from the
+     * dynamically generated number of children.
+     */
     return (
         <div className="parallax">
-            <motion.div className="scroller" ref={scrollerRef} style={{ x }}>
-                {Array.from({ length: numOfChildren }).map((_, index) => (
-                    <span key={index}>{children}</span>
-                ))}
+            <motion.div className="scroller" style={{ x }}>
+                <span>{children}</span>
+                <span>{children}</span>
+                <span>{children}</span>
+                <span>{children}</span>
             </motion.div>
         </div>
     );
@@ -76,8 +69,9 @@ function ParallaxText({ children, baseVelocity = 100 }) {
 
 export default function TechStacks() {
     return (
-        <section className="w-screen min-h-screen font-fira">
-            <ParallaxText baseVelocity={20}>R</ParallaxText>
+        <section className="w-screen fixed flex flex-col space-y-4 font-fira text-6xl uppercase italic font-bold">
+            <ParallaxText baseVelocity={-2}>React Express GraphQL AWS AliCloud MongoDB Nodejs Selenium Cypress Docker Strapi</ParallaxText>
+            <ParallaxText baseVelocity={2}>Next.js Three.js Astro Framer Motion Tailwind Linux SQL NoSQL Git React Native Ionic</ParallaxText>
         </section>
     );
 }
