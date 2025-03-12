7559eec8ca3fae9de2b5bb6765a1b1484f10454d
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2025-03-13 04:40:13 +0800

    upgrade deps

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ const App = lazy(() => import("./App"));
 import "./index.css";
 import Fallback from "./components/Pages/Fallback";
 
-// studio.extend(extension);
-// studio.initialize();
+studio.extend(extension);
+studio.initialize();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
